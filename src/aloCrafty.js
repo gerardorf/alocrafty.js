03c1d5f9186b4e6bd2ef990cd7afe14477d1ee70
Gerardo Rodriguez <gerardorf@gmail.com>
2013-09-06 13:56:55 +0200

    extract openTag and closeTag methods

--- src/aloCrafty.js
+++ src/aloCrafty.js
@@ -1,7 +1,16 @@
 var ALC={}
 
 ALC.tag  = function (tag){
-  return '<'+tag+'></'+tag+'>'
+  return ALC.openTag(tag)
+        +ALC.closeTag(tag);
+}
+
+ALC.openTag = function (tag) {
+  return '<'+tag+'>'
+}
+
+ALC.closeTag = function (tag) {
+  return '</'+tag+'>'
 }
 
 html=function() {
