9c00274259af5648880257cb1dfe7504cd1a1f20
Ricardo García Fernández <ricardogarfe@gmail.com>
2013-12-14 01:32:26 +0100

    Refactoring para la integración continua y análisis estático de código javascript: grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'src/<%= pkg.name %>.min.js'
+      }
+    },
+
+    jshint: {
+      options: {
+        curly: false,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        eqnull: true,
+        browser: true,
+        expr: true,
+        globals: {
+          head: false,
+          module: false,
+          console: false,
+          unescape: false
+        }
+      },
+      files: [ 'Gruntfile.js', 'src/aloCrafty.js' ]
+    },
+
+    jasmine: {
+      all: ['specs/SpecRunner.html'],
+      errorReporting: true
+    },
+
+    watch: {
+      files: '<%= jshint.src %>',
+      tasks: ['jshint']
+    }
+
+  });
+
+  // Dependencies
+  grunt.loadNpmTasks( 'grunt-contrib-qunit' );
+  grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+  grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+  grunt.loadNpmTasks( 'grunt-contrib-watch' );
+  // Jasmine tasks.
+  grunt.loadNpmTasks('grunt-jasmine-html-spec-runner');
+
+  // Default task
+  grunt.registerTask( 'test', [ 'jshint', 'uglify', 'jasmine' ] );
+
+  // Tests
+  grunt.registerTask('default', ['test']);
+};
