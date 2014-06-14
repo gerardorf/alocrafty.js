0f5a72bf258b7171a799ccb35e1260691d1a0fb5
Administrator <admin@local.host>
2014-06-14 23:30:32 +0200

    make changes to accept ricardogarfe's work to integrate with travis-ci

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    uglify: {
+      options: {
+        banner: '/*! aloCrafty.js <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/aloCrafty.js',
+        dest: 'dist/aloCrafty.min.js'
+      }
+    },
+
+    jshint: {
+      options: {
+        curly: false,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        eqnull: true,
+        browser: true,
+        expr: true,
+        globals: {
+          head: false,
+          module: false,
+          console: false,
+          unescape: false
+        }
+      },
+      files: ['src/aloCrafty.js' ]
+    },
+
+    jasmine: {
+      test: {
+        src:'src/*.js', 
+        options: {
+          specs: [
+            'spec/*Spec.js'
+          ],
+        }
+      }
+    }
+  });
+
+  // Load grunt tasks automatically
+  require( 'load-grunt-tasks' )( grunt );
+
+  grunt.registerTask( 'build', [ 'test', 'jshint', 'uglify' ] );
+  grunt.registerTask('test', ['jasmine']);
+  grunt.registerTask('default', ['test']);
+};
