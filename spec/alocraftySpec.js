d3ed59c92dc6f8ca6c3cdab305b447a7ffbbaa17
Administrator <admin@local.host>
2014-06-25 18:20:20 +0200

    allow populate personal namespace

--- spec/alocraftySpec.js
+++ spec/alocraftySpec.js
@@ -1,31 +1,34 @@
 describe("a lo crafty", function() {
 
+  var a = {};
+  aloCrafty.populate(a);
+
   it("generates html tag", function() {
-    expect(html()).toEqual('<html></html>');
+    expect(a.html()).toEqual('<html></html>');
   });
 
   it("generates html tag with arbitrary content", function() {
-    expect(html('arbitrary content')).toEqual('<html>arbitrary content</html>');
+    expect(a.html('arbitrary content')).toEqual('<html>arbitrary content</html>');
   });
 
   it("generates html tag with a nested tag", function() {
     var testHtml = '<html><title>arbitrary title</title></html>';
-    var output =  html(
-                    title('arbitrary title')
+    var output =  a.html(
+                    a.title('arbitrary title')
                   );
     expect(output).toEqual(testHtml);
   });
 
   it("allows adding tag attributes as a map", function() {
     var testHtml = '<body class="test_class" id="main"></body>';
-    var output =  body({class:'test_class', id:'main'});
+    var output =  a.body({class:'test_class', id:'main'});
     expect(output).toEqual(testHtml);
   });
 
   it("allows adding a nested tag with attributes", function () {
     var testHtml='<div class="banner"><span class="title">Jasmine</span></div>';
-    var output =  div({class:'banner'},
-                    span({class:'title'},'Jasmine')
+    var output =  a.div({class:'banner'},
+                    a.span({class:'title'},'Jasmine')
                   );
     expect(output).toEqual(testHtml);
   });
@@ -35,17 +38,17 @@ describe("a lo crafty", function() {
     var url='http://www.becodemyfriend.com';
     var testHTML='<ul id="dl"><li id="dl_osx"><a href="http://www.becodemyfriend.com">OS X</a>(OS X 10.6 or 10.7 is required)</li><li id="dl_win_32"><a href="http://www.becodemyfriend.com">Windows</a> - also available as a <a href="http://www.becodemyfriend.com">portable version</a></li></ul>';
 
-    var output =  ul({id:"dl"},
+    var output =  a.ul({id:"dl"},
 
-                li({id:"dl_osx"},
-                  a({href: url},'OS X'),
+                a.li({id:"dl_osx"},
+                  a.a({href: url},'OS X'),
                   '(OS X 10.6 or 10.7 is required)'
                 ),
 
-                li({id:"dl_win_32"},
-                  a({href: url},'Windows'),
+                a.li({id:"dl_win_32"},
+                  a.a({href: url},'Windows'),
                   ' - also available as a ',
-                  a({href: url},'portable version')
+                  a.a({href: url},'portable version')
                 )
 
               );
@@ -56,16 +59,16 @@ describe("a lo crafty", function() {
   it("can manage unpaired tags including forms, inputs and selects", function() {
     var testHTML='<form method="post" id="authentication" action="https://launchpad.37signals.com/authenticate"><div style="margin:0;padding:0;display:inline"><input type="hidden" value="MRUI2cdi2HaGtG88FOQ2z7ZmMJahFVuojHB/o0Mki4M=" name="authenticity_token"></div><input type="hidden" value="highrise" name="product" id="product"><input type="hidden" value="becode" name="subdomain" id="subdomain"><div class="login_form"><p class="password_entry"><label for="username">Username or email</label><input type="text" name="username" id="username" class="user_name" autocorrect="off" autocapitalize="off"></p></div></form>';
 
-    var output = form({method:'post', id:'authentication', action:'https://launchpad.37signals.com/authenticate'},
-                div({style:'margin:0;padding:0;display:inline'},
-                  input({type:'hidden', value:'MRUI2cdi2HaGtG88FOQ2z7ZmMJahFVuojHB/o0Mki4M=', name:'authenticity_token'})
+    var output = a.form({method:'post', id:'authentication', action:'https://launchpad.37signals.com/authenticate'},
+                a.div({style:'margin:0;padding:0;display:inline'},
+                  a.input({type:'hidden', value:'MRUI2cdi2HaGtG88FOQ2z7ZmMJahFVuojHB/o0Mki4M=', name:'authenticity_token'})
                 ),
-                input({type:'hidden', value:'highrise', name:'product', id:'product'}),
-                input({type:'hidden', value:'becode', name:'subdomain', id:'subdomain'}),
-                div({'class':'login_form'},
-                  p({'class': 'password_entry'},
-                    label({'for':'username'},'Username or email'),
-                    input({type:'text', name:'username', id:'username', 'class':'user_name', autocorrect:'off', autocapitalize:'off'})
+                a.input({type:'hidden', value:'highrise', name:'product', id:'product'}),
+                a.input({type:'hidden', value:'becode', name:'subdomain', id:'subdomain'}),
+                a.div({'class':'login_form'},
+                  a.p({'class': 'password_entry'},
+                    a.label({'for':'username'},'Username or email'),
+                    a.input({type:'text', name:'username', id:'username', 'class':'user_name', autocorrect:'off', autocapitalize:'off'})
                   )
                 )
               );
@@ -75,7 +78,7 @@ describe("a lo crafty", function() {
   it("can manage empty tags like <br>", function() {
     var testHTML='<p>Lorem ipsum<br>and an extra line</p>';
 
-    var output= p('Lorem ipsum',br(),'and an extra line');
+    var output= a.p('Lorem ipsum',a.br(),'and an extra line');
     expect(output).toEqual(testHTML);
     
   });
@@ -83,9 +86,8 @@ describe("a lo crafty", function() {
   it("can manage comments", function() {
     var testHTML='<p>Lorem ipsum<!--this is a comment-->dolor sit amet</p>';
 
-    var output= p('Lorem ipsum',comment('this is a comment'),'dolor sit amet');
+    var output= a.p('Lorem ipsum',a.comment('this is a comment'),'dolor sit amet');
     expect(output).toEqual(testHTML);
     
   });
-
 });
--- src/alocrafty.js
+++ src/alocrafty.js
@@ -1,114 +1,119 @@
-var alc= (function (){
+var aloCrafty = (function (){
+  var _renderer= (function (){
 
-  var _openMarkUp = '<';
-  var _closeMarkUp = '>';
-  var _unpariedTags = ['input'];
+    var _openMarkUp = '<';
+    var _closeMarkUp = '>';
+    var _unpariedTags = ['input'];
 
-  function _tag(tag, args){
-    return _openMarkUp+
-          tag+
-          _serializeAttributes(args)+
-          _closeMarkUp+
-          _serializeContent(args)+
-          _closeTag(tag);
-  }
-
-  function _serializeAttributes(atts){
-    var attributes = _extractAttributes(atts);
-    return _writeAttributes(attributes);
-  }
-
-  function _extractAttributes(atts){
-    var attributes='';
-    if (typeof atts[0]==='object'){
-      attributes=Array.prototype.shift.call(atts);
+    function _tag(tag, args){
+      return _openMarkUp+
+            tag+
+            _serializeAttributes(args)+
+            _closeMarkUp+
+            _serializeContent(args)+
+            _closeTag(tag);
     }
-    return attributes;
-  }
 
-  function _writeAttributes(atts){
-    var output='';
-    for (var att in atts){
-      output += ' '+att+'="'+atts[att]+'"' ;
+    function _serializeAttributes(atts){
+      var attributes = _extractAttributes(atts);
+      return _writeAttributes(attributes);
     }
-    return output;
-  }
 
-  function _serializeContent(content){
-    var output = '';
-    for (var i = 0, len = content.length; i < len; i++) {
-      output += content[i];
+    function _extractAttributes(atts){
+      var attributes='';
+      if (typeof atts[0]==='object'){
+        attributes=Array.prototype.shift.call(atts);
+      }
+      return attributes;
     }
-    return output;
-  }
 
-  function _closeTag(tag) {
-    var output = ''; 
-    if (_isPaired(tag)) {output = '</'+tag+'>';}
-    return output;
-  }
+    function _writeAttributes(atts){
+      var output='';
+      for (var att in atts){
+        output += ' '+att+'="'+atts[att]+'"' ;
+      }
+      return output;
+    }
 
-  function _isPaired(tag) {
-    return (_unpariedTags.indexOf(tag)<0);
-  }
+    function _serializeContent(content){
+      var output = '';
+      for (var i = 0, len = content.length; i < len; i++) {
+        output += content[i];
+      }
+      return output;
+    }
+
+    function _closeTag(tag) {
+      var output = ''; 
+      if (_isPaired(tag)) {output = '</'+tag+'>';}
+      return output;
+    }
+
+    function _isPaired(tag) {
+      return (_unpariedTags.indexOf(tag)<0);
+    }
+
+    return{
+      tag: _tag
+    };
+
+  })();
+
+  var _populate = (function(){
+
+    function _generateRegularTag(tag, namespace) {
+      namespace[tag]  = function(){
+        return _renderer.tag(tag, arguments);
+      };
+    }
+
+    function _populateRegularTags(namespace) {
+      var tags=['a','abbr','acronym','address','applet','area','article','aside','audio','b','base','basefont','bdi','bdo','big','blockquote','body','button','anvas','caption','center','cite','code','col','colgroup','command','datalist','dd','del','details','dfn','dialog','dir','div','dl','dt','em','embed','fieldset','figcaption','figure','font','footer','form','frame','frameset','head','header','hgroup','h1','h2','h3','h4','h5','h6','hr','html','i','iframe','img','input','ins','kbd','keygen','label','legend','li','link','main','map','mark','menu','meta','meter','nav','noframes','noscript','object','ol','optgroup','ption','output','p','param','pre','progress','q','rp','rt','ruby','s','samp','script','section','select','small','source','span','strike','strong','tyle','sub','summary','sup','table','tbody','td','textarea','tfoot','th','thead','time','title','tr','track','tt','u','ul','var','video','wbr'];
+      for(var i in tags){
+        _generateRegularTag(tags[i], namespace);
+      }
+    }
+
+    function _generateEmptyTags(tag, namespace) {
+      namespace[tag] = function(){
+        return '<' + tag + '>';
+      };
+    }
+
+    function _populateEmptyTags(namespace) {
+      var tags=['br'];
+      for(var i in tags){
+        _generateEmptyTags(tags[i], namespace);
+      }
+    }
+
+    function _generateCommentTag(tag, namespace){
+      namespace[tag] = function (text) {
+        return '<!--'+text+'-->';
+      };
+    }
+
+    function _populateCommentTag(namespace) {
+      var tags=['comment'];
+      for(var i in tags){
+        _generateCommentTag(tags[i], namespace);
+      }
+    }
+
+    function _populateHTMLTags(namespace) {
+      _populateRegularTags(namespace);
+      _populateEmptyTags(namespace);
+      _populateCommentTag(namespace);
+    }
+
+    return{
+      populateHTMLTags: _populateHTMLTags
+    };
+
+  })();
 
   return {
-    tag: _tag
+    populate: _populate.populateHTMLTags
   };
 
 })();
-
-var tags = (function(){
-  function _generateRegularTag(tag) {
-    window[tag]  = function(){
-      return alc.tag(tag, arguments);
-    };
-  }
-
-  function _populateRegularTags() {
-    console.log('try to generate tags');
-    var tags=['a','abbr','acronym','address','applet','area','article','aside','audio','b','base','basefont','bdi','bdo','big','blockquote','body','button','anvas','caption','center','cite','code','col','colgroup','command','datalist','dd','del','details','dfn','dialog','dir','div','dl','dt','em','embed','fieldset','figcaption','figure','font','footer','form','frame','frameset','head','header','hgroup','h1','h2','h3','h4','h5','h6','hr','html','i','iframe','img','input','ins','kbd','keygen','label','legend','li','link','main','map','mark','menu','meta','meter','nav','noframes','noscript','object','ol','optgroup','ption','output','p','param','pre','progress','q','rp','rt','ruby','s','samp','script','section','select','small','source','span','strike','strong','tyle','sub','summary','sup','table','tbody','td','textarea','tfoot','th','thead','time','title','tr','track','tt','u','ul','var','video','wbr'];
-    for(var i in tags){
-      _generateRegularTag(tags[i]);
-    }
-  }
-
-  function _generateEmptyTags(tag) {
-    window[tag] = function(){
-      return '<' + tag + '>';
-    };
-  }
-
-  function _populateEmptyTags() {
-    var tags=['br'];
-    for(var i in tags){
-      _generateEmptyTags(tags[i]);
-    }
-  }
-
-  function _generateCommentTag(tag){
-    window[tag] = function (text) {
-      return '<!--'+text+'-->';
-    };
-  }
-
-  function _populateCommentTag() {
-    var tags=['comment'];
-    for(var i in tags){
-      _generateCommentTag(tags[i]);
-    }
-  }
-
-  function _populateHTMLTags() {
-    _populateRegularTags();
-    _populateEmptyTags();
-    _populateCommentTag();
-  }
-
-  return {
-    populateHTMLTags: _populateHTMLTags
-  };
-
-})();
-
-tags.populateHTMLTags();
